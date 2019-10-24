841ff7da2ee7d1f7e07faeadd619c1c2e19cc895
Nicolas <nicolas@elsemillerocoworking.com>
2019-10-24 18:42:30 -0300

    findia

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,15 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @dose.save
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_15_202104) do
+ActiveRecord::Schema.define(version: 2019_10_24_170856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 2018_06_15_202104) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo"
   end
 
   create_table "doses", force: :cascade do |t|
